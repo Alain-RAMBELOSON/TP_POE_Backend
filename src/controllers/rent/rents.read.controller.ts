f0086411c49939433b88205d14a05f8880f5d7ae
Alain RAMBELOSON <alain.rambeloson@gmail.com>
2022-09-30 09:34:12 +0200

    fix: Get only IDs fromget all entities requests

--- src/controllers/rent/rents.read.controller.ts
+++ src/controllers/rent/rents.read.controller.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 const Rent = require("../../models/rent.model");
 
 module.exports.getRents = async (request: Request, response: Response) => {
-  const customers = await Rent.find({});
+  const customers = await Rent.find({}, "_id");
   response.send(customers);
 };
--- src/controllers/vehicle/vehicles.read.controller.ts
+++ src/controllers/vehicle/vehicles.read.controller.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 const Vehicle = require("../../models/vehicle.model");
 
 module.exports.getVehicles = async (request: Request, response: Response) => {
-  const vehicles = await Vehicle.find({});
+  const vehicles = await Vehicle.find({}, "_id");
   response.send(vehicles);
 };
