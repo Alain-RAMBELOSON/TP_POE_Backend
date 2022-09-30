7d17090d751757ad02e9c82a1665522fe6e3415f
Alain RAMBELOSON <alain.rambeloson@gmail.com>
2022-09-30 05:44:19 +0200

    fix: git cors browser error

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
 const express = require("express");
 const connector = require("../database/connector");
 const Database = require("../database/database");
+
+const cors = require("cors");
+
 const vehicleRouter = require("../routes/vehicle.routes");
 const customerRouter = require("../routes/customer.routes");
 const rentRouter = require("../routes/rent.routes");
@@ -9,6 +12,7 @@ const app = express();
 
 connector.connect(Database.MongoDB);
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/vehicle", vehicleRouter);
